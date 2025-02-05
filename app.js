28150d0a958ccf2174a6eacd5c597fe49eb82311
SophyRJ <sofiarojasc5@gmail.com>
2025-02-05 20:11:58 +0100

    finalizacion challenge

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//creacion de la lista vacia
 let amigos = []
 
+//llamamos a la funcion actualizar lista
 actualizarLista()
+
+//funcion agregar amigo
 function agregarAmigo(){
     let amigo= document.querySelector('input')
     if(amigo.value ===""){
         alert("Rellene el campo")
     }else{
         amigos.push(amigo.value);
+        console.log(amigos)
         amigo.value=""
-        
     }
 }
 
+//funcion actualizar lista
 function actualizarLista(){
     let lista= document.getElementById('listaAmigos')
     lista.innerHTML=""
@@ -20,10 +24,12 @@ function actualizarLista(){
         let li=document.createElement('li')
         li.textContent= amigos[i]
         lista.appendChild(li)
+        
     }
+    
 }
 
-
+//funcion sortear amigo
 function sortearAmigo(){
     if(amigos.length==0){
         alert("La lista esta vacia")
